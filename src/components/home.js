31c8ee7b7b75136250c1ba3c8c9d48d714ea33d4
KalaQuest <malakooku@gmail.com>
2018-07-17 09:35:29 -0600

    added card and input components

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Header from './header';
+import Card from './card';
  class Home extends Component {
   render() {
     return (
       <div className="home">
         { Header('Bt Madlibs', 'fill out the fields to complete the madlibs story') }
-        { Header() }
+        <Card/>
       </div>
     );
   }
